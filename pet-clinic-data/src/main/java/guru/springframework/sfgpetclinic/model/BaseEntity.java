44e5717b67a0e81f65dd56b4501e9caf274e5413
Marco Gordillo <marcopgordillo@gmail.com>
2018-10-26 12:19:01 -0500

    Adding Serializable to Base Entity. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -6,10 +6,11 @@ import org.springframework.data.annotation.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
 @Data
 @MappedSuperclass
-public class BaseEntity {
+public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
