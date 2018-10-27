e851a532fa4b8c7a705deb60d9b73da1715831a6
Marco Gordillo <marcopgordillo@gmail.com>
2018-10-27 11:00:12 -0500

    Added PetType Service. Closes #29

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -4,18 +4,10 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-
 @Service
 public class OwnerServiceMap
         extends AbstractMapService<Owner, Long>
         implements OwnerService {
-
-
-    @Override
-    public Set<Owner> findAll() {
-        return super.findAll();
-    }
 
     @Override
     public Owner findByLastName(String lastName) {
