4c2dad5ff20778ac50e3a6b6e8efb386cecd196d
Marco Gordillo <marcopgordillo@gmail.com>
2018-10-29 09:37:11 -0500

    Adding ownerDisplay controller, view and tests. #53

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
@@ -21,6 +22,12 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping({"/{id}"})
+    public String showOwner(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("owner", ownerService.findById(id));
+        return "owners/ownerDetails";
     }
 
     @RequestMapping("/find")
